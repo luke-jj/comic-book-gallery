ea5622a6372289ecaf4b4077e7a572f3e55f2f0a
LukeJ <infiLuca@gmail.com>
2019-01-25 15:23:38 +0100

    add more comic book data to repository

--- Data/ComicBookRepository.cs
+++ Data/ComicBookRepository.cs
@@ -25,8 +25,32 @@ namespace comic_book_gallery.Data
                     new Artist() { Name = "Edgar Delgado", Role = "Colors" },
                     new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
                 }
+            },
+            new ComicBook() {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Favorite = false,
+                Artists = new Artist[] {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+                    new Artist() { Name = "Marcos Martin", Role = "Inks" },
+                    new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+                    new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+                }
+            },
+            new ComicBook() {
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                Favorite = false,
+                Artists = new Artist[] {
+                    new Artist() { Name = "Jeff Smith", Role = "Script" },
+                    new Artist() { Name = "Jeff Smith", Role = "Pencils" },
+                    new Artist() { Name = "Jeff Smith", Role = "Inks" },
+                    new Artist() { Name = "Jeff Smith", Role = "Letters" }
+                }
             }
-            // TODO: add two more comic books
         };
 
         public ComicBook[] GetComicBooks() {
